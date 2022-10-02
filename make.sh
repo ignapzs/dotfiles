0c371db722dad813a81599b8b9f924cefbc34b99
ignacio <ignacio@ignapzs.net>
2022-10-02 20:54:19 +0200

    feat(make): first steps into initial script

---
+++ make.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+############################
+# .make.sh
+# First steps
+############################
+
+########## Variables
+#projectDir=~/Projects/github/dotfiles 	# dotfiles github directory
+backupDir=~/Backup/dotfiles_backup      # old/existing dotfiles backup directory
+
+backupFiles () {
+	echo -n "Creating $backupDir for backup of any existing dotfiles ..."
+	mkdir -p $backupDir
+	echo "done"
+}
+
+clear
+backupFiles;
