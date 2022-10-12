b0d3fc1299e14a4d78fcd459326a9af4945e5ea1
ignacio <ignacio@ignapzs.net>
2022-10-12 20:03:47 +0200

    feat(script): further steps in make shell script

--- make.sh
+++ make.sh
@@ -1,18 +1,50 @@
 #!/bin/bash
 ############################
 # .make.sh
-# First steps
+# More advanced version
 ############################
 
 ########## Variables
-projectDir=~/Projects/github/dotfiles 	# dotfiles github directory
-backupDir=~/Backup/dotfiles_backup      # old/existing dotfiles backup directory
+userDir=~
+projectDir=$userDir/Projects/github/dotfiles 	# dotfiles github directory
+backupDir=$userDir/Backup/dotfiles_backup     # old/existing dotfiles backup directory
+date=$(date +"%Y%m%d%H%M%S")
+# list of files/folders to symlink in homedir
+dotFiles="alias antigenrc bashrc gitconfig p10k.zsh zshrc"
+
+
+itsDone () {
+  echo "done"
+}
+
+tick () {
+  echo -e "\xE2\x9C\x94" | iconv -f UTF-8
+}
 
 backupFiles () {
-	echo -n "Creating $backupDir for backup of any existing dotfiles ..."
+	echo "Creating $backupDir for backup of any existing dotfiles ..."
 	mkdir -p $backupDir
-	echo "done"
+	echo "Backup of files ..."
+
+  for file in $dotFiles; do
+      echo -n "$file "
+      cp ~/."$file" $backupDir/."$file"."$date"
+      tick
+  done
+
+}
+
+downloadTheProject () {
+  dotfilesGitHubURL="https://github.com/ignapzs/dotfiles"
+  echo "Downloading dotfiles project from $dotfilesGitHubURL ..."
+  cd $userDir/Projects/github/ || exit 1
+  git clone $dotfilesGitHubURL
+  itsDone
 }
 
 clear
 backupFiles;
+downloadTheProject;
+#installRequiredSW;
+#performLinking;
+#performCheckings;
